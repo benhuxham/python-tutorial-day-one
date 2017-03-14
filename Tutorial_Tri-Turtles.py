009a9e537f18731a5c615fbe414ebbb55eb488c4
Ben Huxham <benhuxham@gmail.com>
2017-03-14 21:56:06 +1000

    Entered Exercise TriditUn

---
+++ Tutorial_Tri-Turtles.py
@@ -0,0 +1,14 @@
+# http://openbookproject.net/thinkcs/python/english3e/hello_little_turtles.html
+
+# Exercise Tri dit Un
+#
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forward(50)
+alex.left(90)
+alex.forward(30)
+
+wn.mainloop()
