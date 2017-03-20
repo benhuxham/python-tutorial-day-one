6bd6f5a6daf8037055b1c40b0c8e01a499a645c5
Ben Huxham <benhuxham@gmail.com>
2017-03-20 23:18:32 +1000

    Introduced loop

---
+++ Tutorial_Exercise Tri dit tri-Turtles.py
@@ -0,0 +1,26 @@
+# Example 3 dit hex
+import turtle
+
+wn = turtle.Screen()
+wn_bgcolour = "lightgreen"
+
+tess = turtle.Turtle()
+tess_shape = "turtle"
+tess_colour = "blue"
+tess_size = 20
+tess_increase = .3
+tess_forward = tess_size
+tess_right = 24
+tess_steps = 80
+
+tess.shape(tess_shape)
+tess.color(tess_colour)
+tess.penup()
+
+for i in range(tess_steps):
+    tess.stamp()
+    tess_forward = tess_forward + tess_increase
+    tess.forward(tess_forward)
+    tess.right(tess_right)
+
+wn.mainloop()
