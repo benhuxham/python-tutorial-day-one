62e6abcb45dea5b9060bf3e1a526800c1e64e988
Ben Huxham <benhuxham@gmail.com>
2017-03-20 22:29:21 +1000

    Introduced Variables

--- Tutorial_Exercise Tri-Turtles.py
+++ Tutorial_Exercise Tri-Turtles.py
@@ -5,17 +5,21 @@
 import turtle
 
 wn = turtle.Screen()
-wn.bgcolor("lightgreen")
+
+turtle_background_colour = input("What colour would you like the background to be? ")
+tess_colour = input("What colour would you like the Tess to be? ")
+tess_pen_width = int(input("What width would you like the pen to be? "))
+
+wn.bgcolor(turtle_background_colour)
 wn.title("Hello, Tess")
 alex = turtle.Turtle()
-
 alex.forward(50)
 alex.left(90)
 alex.forward(30)
 
 tess = turtle.Turtle()
-tess.color("blue")
-tess.pensize(3)
+tess.color(tess_colour)
+tess.pensize(tess_pen_width)
 
 tess.forward(50)
 tess.left(120)
