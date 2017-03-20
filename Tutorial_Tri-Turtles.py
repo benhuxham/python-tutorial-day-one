56bdde0a4d71e17935a403dbedc0533a7df8f359
Ben Huxham <benhuxham@gmail.com>
2017-03-20 22:11:19 +1000

    Pre-Rename

--- Tutorial_Tri-Turtles.py
+++ Tutorial_Tri-Turtles.py
@@ -5,10 +5,20 @@
 import turtle
 
 wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Hello, Tess")
 alex = turtle.Turtle()
 
 alex.forward(50)
 alex.left(90)
 alex.forward(30)
+
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(3)
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
 
 wn.mainloop()
